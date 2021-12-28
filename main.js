ac61ca42e6e0ec92f0de35c96086e7008e71d745
saiprocoder <95332916+saiprocoder@users.noreply.github.com>
2021-12-28 10:40:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-var a=prompt ("Welcome to our Project of Perimeter and Area! If you want to play a quiz, type Q. If you want to see slides, type S. If you want to see a video on Perimeter, type VP. If you want to see a video on Area, type VA. If you want to see the project, type P.");
-
-              if (a=="Q"){
-                window.location.href=""
-              }
-
-              if (a=="S"){
-                window.location.href=""
-              }
-
-              if (a=="VP"){
-                window.location.href="vp.mp4"
-              }
+var a=prompt ("Welcome to our Project of Perimeter and Area! If you want to see a video on Area, type VA. If you want to see the project, type P.");
 
               if (a=="VA"){
                 window.location.href="va.mp4"
